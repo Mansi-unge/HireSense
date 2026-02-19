7237b38f6b90445b799f3c9e7e9d3c29982df718
Mansi Unge <mansiunge@gmail.com>
2026-02-19 21:43:20 +0530

    Downgrade Python to 3.13 for Render

--- backend/app.py
+++ backend/app.py
@@ -139,6 +139,7 @@ def dashboard():
     return render_template("dashboard.html", analyses=last_analyses)
 
 
-# ---------------- RUN FLASK APP ----------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
